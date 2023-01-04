0f19bbce0016659554cbccfe3e867a3ff48e5254
André Dorigueto Canal <Andre.Canal@megawork.local>
2023-01-04 15:34:24 -0300

    random income and dates generation file

---
+++ generate_random_income_and_dates.R
@@ -0,0 +1,25 @@
+# Generate random income figures
+paste(random_figures <- data.frame(sample(1000:20000000, 260)))
+
+# Generate random income dates
+## 1st Option
+date <- sample(seq.Date(as.Date('2022-01-01'), as.Date('2023-12-31'), by="day"), 260)
+paste(sort(date))
+
+## 2nd Option
+rdate <- function(n,
+                  min = paste0(format(Sys.Date(), '%Y'), '-01-01'),
+                  max = paste0(format(Sys.Date(), '%Y'), '-12-31'),
+                  sort = TRUE) {
+  
+  dates <- sample(seq(as.Date(min), as.Date(max), by = "day"), n, replace = TRUE)
+  if (sort == TRUE) {
+    sort(dates)
+  } else {
+    dates
+  }
+  
+}
+
+dates <- data.frame(rdate(260, '2022-01-01', '2023-12-31'))
+paste(dates)
